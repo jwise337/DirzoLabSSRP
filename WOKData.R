d0bade4ebe2678c6110276fe1d7be07b7941f2ea
jwise3 <jwise3@stanford.edu>
2020-07-17 08:31:23 -0700

    WOKData.R

--- WOKData.R
+++ WOKData.R
@@ -12,7 +12,7 @@ wdi <- read.csv("WDI_data.csv")
 # From https://github.com/ropensci/refsplitr
 # load the Web of Science records into a dataframe
 
-dat1 <- references_read(data = "2000.txt")
+dat1 <- references_read(data = "1940.txt")
   
 
 
@@ -20,25 +20,34 @@ dat1 <- references_read(data = "2000.txt")
 dat2 <- authors_clean(references = dat1)
 
 
+
 # after revieiwng disambiguation, merge any necessary corrections
 dat3 <- authors_refine(dat2$review, dat2$prelim)
 
-dat3$year <- 2000
-write.csv(dat3, "2000.csv")
-head(2000)
-dat3 <- read.csv ("2000.csv")
-w
+dat3$year <- 1940
+
+write.csv(dat3, "1940.csv")
+head(1940)
+dat3 <- read.csv ("1940.csv")
+
 sub <- sample_n(dat3, 10)
 
 # georeference the author locations
 dat4 <- authors_georef(dat3)
 
-dat4$year <- 2020
-write.csv(dat4, "dat4.csv")
-head(dat3)
-dat3 <- read.csv ("dat3.csv")
+
 
 # generate a map of coauthorships; this is only one of the five possible visualizations  
 plot_net_address(dat4$addresses)
 
-read_csv("1921.csv")
+
+
+dat <- rbind("WOK2020", "dat3")
+
+ggplot (dat, aes (x = year, fill = country)) + geom_bar (position = "fill") 
+
+ggplot (WOK2020, aes (x = year, fill = country)) + geom_bar (position = "fill") 
+
+
+
+
